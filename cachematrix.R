c8d118cfa7db2e2ac18287f729cd70a9b2de5d9a
natemoser <nathan.moser@gmail.com>
2014-07-26 20:45:25 -0500

    fix up comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,14 @@
 ## cachematrix.R
 ## Nathan Moser (nate@rezonate.org)
 ##
-## Implements an object that calculates the inverse of a supplied matrix, 
-## and caches the result in the object. If the object is used again with the 
-## same input matrix, the cached result will be returned, rather than the
+## Implements an object can be used to calculate the inverse of a supplied matrix, 
+## and caches the result in the object. If the object is used again to find
+## the inverted matrix, the cached result will be returned, rather than the
 ## inverse being calculated again.
 
 ## makeCacheMatrix: accepts a matrix as input, and returns
 ## an object with functions for getting and setting the cached matrix, as
-## well as getting and calculating/setting the inverse matrix.
+## well as getting and setting the inverted matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -26,7 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## cacheSolve: accepts an object that was created via makeCacheMatrix, and 
-## if the inverse of the matrix has previously calculated, returns that
+## if the inverse of the matrix has been previously calculated, returns that
 ## cached result. Otherwise, the inverse of the matrix is calcuated, stored
 ## in the object passed in to cacheSolve, and then the inverse matrix is 
 ## returned. It is assumed that the matrix supplied is always invertable.
